5ca66a6848eb8458f2f5a8b28ecb613d378289e2
asadah701 <76811517+asadah701@users.noreply.github.com>
2021-11-19 12:56:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var imageSrc = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", imageSrc);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var imageSrc2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", imageSrc2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "🎊 Player 1 Wins!";
+}else if(randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🎊";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
+
